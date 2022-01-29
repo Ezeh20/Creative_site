842772c6b80759af6a234ad50db0dc798d12f21e
unknown <buchimmaduabuchukwu@gmail.com>
2022-01-29 13:35:48 +0100

    initial commit

---
+++ style.css
@@ -0,0 +1,7 @@
+@import url('https://fonts.googleapis.com/css2?family=Mochiy+Pop+P+One&family=Montserrat+Alternates:wght@100&family=Press+Start+2P&display=swap');
+
+/**    font-family: 'Mochiy Pop P One', sans-serif;
+
+    font-family: 'Montserrat Alternates', sans-serif;
+
+    font-family: 'Press Start 2P', cursive; */
